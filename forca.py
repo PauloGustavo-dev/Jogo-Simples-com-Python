19e43514cc590e2969da6ee27b9c43d5b6d9f814
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-03-15 20:38:57 -0300

    Começando a implementação do jogo forca

--- forca.py
+++ forca.py
@@ -3,6 +3,20 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou=False
+    acertou=False
+    #looping, enquanto ele nao acerta e enquanto ele nao foi "enforcado"
+    while(not enforcou and not acertou):
+        chute= input("Insira uma letra: ")
+
+        index=0
+        for letra in palavra_secreta:
+            if(chute.upper()==letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute,index))
+            index+=1
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
