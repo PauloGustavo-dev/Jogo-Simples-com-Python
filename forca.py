4c884658825a4647d2dd37922ef7ca8bc98279f7
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-03-15 22:42:52 -0300

    refinando o codigo com funções

--- forca.py
+++ forca.py
@@ -1,28 +1,20 @@
 import random
+
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
-
-    arquivo = open("palavras.txt","r")
-    palavras=[]
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
     
-    arquivo.close()
+    mensagem_de_abertura()
 
-    numero= random.randrange(0,len(palavras))
-    palavra_secreta = palavras[numero].upper()
-    
+    palavra_secreta=carrega_palavra_secreta()
+
+    #lista de caracteres que serão substituidos conforme o usuario acerta 
     letras_acertadas = ["_" for letra in palavra_secreta]
+    print(letras_acertadas)
 
     enforcou=False
     acertou=False
     erros=0
 
-    #looping, enquanto ele nao acerta e enquanto ele nao foi "enforcado"
+    #looping, jogo roda enquanto ele nao acerta e enquanto ele nao foi "enforcado"
     while(not enforcou and not acertou):
         chute= input("Insira uma letra: ")
         chute= chute.strip().upper()
@@ -43,9 +35,27 @@ def jogar():
     if(acertou):
         print("Parabens, voce ganhou")
     else:
-        print("Voce perdeu")
-         
-    print("Fim do jogo")
+        print("Voce perdeu, a palavra era",palavra_secreta)
+
+def mensagem_de_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt","r")
+    palavras=[]
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+
+    numero= random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
 
 if(__name__ == "__main__"):
     jogar()
