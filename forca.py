b0216b2160197e9988bdf8e75a7a7165b3c8b06f
Paulo Gustavo Sousa Brito <oluap077@gmail.com>
2023-03-15 21:48:38 -0300

    Jogo funcionando, função de quantidade de tentativas, melhor apresentação do resultado de cada rodada,mensagem conforme resultado do jogo

--- forca.py
+++ forca.py
@@ -3,20 +3,36 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou=False
     acertou=False
+    erros=0
+
     #looping, enquanto ele nao acerta e enquanto ele nao foi "enforcado"
     while(not enforcou and not acertou):
         chute= input("Insira uma letra: ")
+        chute= chute.strip().upper()
+        
+        if(chute in palavra_secreta):
+            index=0
+            for letra in palavra_secreta:
+                if(chute==letra):
+                    letras_acertadas[index]=letra
+                index+=1
+        else:
+            erros+=1
 
-        index=0
-        for letra in palavra_secreta:
-            if(chute.upper()==letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(chute,index))
-            index+=1
+        enforcou = erros==6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
+    if(acertou):
+        print("Parabens, voce ganhou")
+    else:
+        print("Voce perdeu")
+         
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
